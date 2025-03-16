ae5624d47ecec48500040a5a805703763d1063ff
V_Hello-Kitty_V <strangetip@gmail.com>
2025-03-17 01:32:53 +0200

    render prop components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import React from "react";
-
+import React, { useState } from "react";
 import List from "./List";
+import Currency from "./Currency";
 
 export default function App() {
-  return (
-    <>
-      <List />
-    </>
-  );
-}
+  
+  return(
+    <div className="wrapper">
+    < Currency/>
+    <List/>
+    </div>
+ 
+  )
+
+};
---
+++ src/Currency.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+//render prop components
+export default function Currency() {
+
+  return (
+    <div>
+      <Amount currency={{ name: "Euro", component: Euro }} />
+      <Amount currency={{ name: "Pound", component: Pound }} />
+    </div>
+  );
+}
+const Euro = ({ amount}) => <p className="font-bold">Euro: {(amount * 0.95).toFixed(2)} </p>;
+const Pound = ({ amount }) => <p className="font-bold" >Pound: {(amount * 0.95).toFixed(2)} </p>;
+
+const Amount = ({ currency }) => {
+  const [amount, setAmount] = useState(0);
+  const CurrencyComponent = currency.component;
+
+  return (
+    <>
+      <h1>US Dollar to {currency.name}</h1>
+      <Button onClick={() => setAmount(amount + 1)}>+</Button>
+      <Button disabled={amount === 0} onClick={() => setAmount(amount - 1)}>-</Button>
+
+      <p className="font-bold">US Dollar: {amount}</p>
+      <CurrencyComponent amount={amount}/>
+    </>
+  );
+};
+
+const Button=({children, ...others})=>{
+    return(
+        <button className="btn btn-neutral mr-2" {...others} >{children}</button>
+    )
+}
